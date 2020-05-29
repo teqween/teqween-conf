9cff99199d68204723b12f79aebe992c1135cd24
Mohamed El Amine DADDOU <mea.daddou@gmail.com>
2020-05-29 09:33:33 +0100

    add expressjs

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(path.join(__dirname, 'dist'), 'index.html'));
 });
 
-app.listen(port);
+var server = app.listen(port, function() {
+  console.log('listening on port ', port);
+});
