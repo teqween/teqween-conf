29b59875cb57d99558796bb459ce6c883a8362ce
Mohamed El Amine DADDOU <mea.daddou@gmail.com>
2020-05-29 09:17:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import Peer from 'peerjs';
+
+window.onload = function() {
+    // DOM Elements
+    const webcam = document.getElementById('webcam');
+    const myid = document.getElementById('myid');
+    const callnow = document.getElementById('callnow');
+
+    // Creating Peer
+    const peer = new Peer({
+        host: 'teqween-webrtc.herokuapp.com',
+        secure: true,
+        config: {
+            iceServers: [
+                { url: 'stun:turn.teqween.com' },
+                { url: 'turn:turn.teqween.com', username: 'teqween', credential: 'teqween'}
+            ]
+        }
+    })
+
+    // Getting My ID
+    peer.on('open', function(id) {
+        myid.innerText =  `MY ID: ${id}`;
+    })
+
+    // Initiate a call
+    callnow.addEventListener('click', function() {
+        const friendsId = prompt('Your Friend ID');
+        
+        navigator.getUserMedia({ video: true, audio: false }, function(stream) {
+            const call = peer.call(friendsId, stream);
+
+            call.on('stream', function(stream) {
+                webcam.srcObject = stream;
+                webcam.play();
+            })
+        }, function() {})
+        
+    })
+
+    // Receive a call
+    peer.on('call', function(call) {
+        navigator.getUserMedia({ video: true, audio: false }, function(stream) {
+            call.answer(stream);
+
+            call.on('stream', function(stream) {
+                webcam.srcObject = stream;
+                webcam.play();
+            })
+        }, function() {})
+    })
+
+}
