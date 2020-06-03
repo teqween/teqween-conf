19cfa23d0b0d9d5c8ce35e1e52cafdfcb4a5118c
Mohamed El Amine DADDOU <mea.daddou@gmail.com>
2020-06-03 09:14:10 +0100

    video 3 - get ready

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         use: ['html-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg|mp3)$/i,
         use: [
           {
             loader: 'file-loader',
